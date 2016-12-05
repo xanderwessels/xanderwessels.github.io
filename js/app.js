f21e553fc7801b3f10eed665443fb45dcc46dedd
XanderWessels <xanwes97@hotmail.com>
2016-12-05 23:39:24 +0100

    -

--- js/app.js
+++ js/app.js
@@ -6,28 +6,41 @@ function replaceAll(str, find, replace) {
 }
 
 function loadImages(subreddit) {
+    var count = 0;
 
     $.getJSON("https://www.reddit.com/r/" + subreddit + "/.json?jsonp=?", function(data) {
-        $.each(data.data.children.slice(0,8), function(i,item){
-            var str1 = replaceAll(item.data.url, "&amp;", "&")
-            if (str1.search(".jpg") < 0){ str1 = str1 + ".jpg";}
-            if (item.data.domain == "flickr.com"){str1 = "placeholder.png"}
-            images.push(str1);
-            console.log(str1,i);
-            console.log(item.data)
-            var $image =  $("#img" + i);
-            $("#img" + i).attr("src", str1)
+        $.each(data.data.children.slice(0,16), function(i,item){
+            if(item.data.post_hint == "image" && item.data.domain != "flickr.com"){
+                var str1 = replaceAll(item.data.url, "&amp;", "&")
+                if (str1.search(".jpg") < 0) {
+                    str1 = str1 + ".jpg";
+                }
+                images.push(str1);
+                //console.log(str1,i);
+                //console.log(item.data)
+                var $image = $("#img" + count);
+                $("#img" + count).attr("src", str1)
 
-            var title = replaceAll(item.data.title,/\[.*\]/,"")
-            $($image).siblings().find("h2").text(title);
-            $($image).siblings().find("a").attr("href", item.data.url).text("Click here to enlarge");
+                var title = replaceAll(item.data.title, /\[.*\]/, "")
+                $($image).siblings().find("h2").text(title);
+                $($image).siblings().find("a").attr("href", item.data.url).text("Click here to enlarge");
+                count++;
+            } else {
+                console.log(item.data.title);
+            }
         })
+        count = 0;
+        console.log(images);
         console.log(data);
     })
         .done(function() { console.log('getJSON request succeeded!'); })
-        .fail(function(jqXHR, textStatus, errorThrown) { console.log('getJSON request failed! ' + textStatus); success = false; })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            console.log('getJSON request failed! ' + textStatus);
+            success = false;
+        })
         .always(function() { console.log('getJSON request ended!'); });
 }
+
 $("form").submit(function( event ) {
     loadImages($("input:first").val());
 
@@ -35,4 +48,4 @@ $("form").submit(function( event ) {
 });
 
 
-loadImages("earthporn");
+loadImages("pics");
